742bac50265cdd1a45c401645cf6ed93f7f1d4e6
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-04 22:20:57 +0100

    changes

--- index.js
+++ index.js
@@ -11,9 +11,23 @@ console.log('hello');
 //     console.log(property);
 // }
 
-fetch('https://jsonplaceholder.typicode.com/comments')
-.then(function(res){
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((res)=>{
      return res.json();
-}).then(function(data){
+}).then((data)=>{
     console.log(data);
 })
+
+const data={
+    title:"This is title",
+    body:"This is the post body",
+    userId:0
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts',{
+    method:"POST",
+    body:JSON.stringify(data),
+    headers:{
+        "content-type":"application/json"
+    }
+}).then(res => res.json()).then(data => console.log(data));
